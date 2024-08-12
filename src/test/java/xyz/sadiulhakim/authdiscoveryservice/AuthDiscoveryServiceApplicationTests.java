3aab9959a44be1115bd0ec99a96db8768fede600
Sadiul Hakim <sadiulhakim@gmail.com>
2024-08-12 22:43:54 +0600

    Start

---
+++ src/test/java/xyz/sadiulhakim/authdiscoveryservice/AuthDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.sadiulhakim.authdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
